7e0ee9fd83d17bcc60c9c8a9e819758228d70d0e
gbourguett <gbourguett@lovalledor.cl>
2024-12-18 13:46:12 -0300

    ==

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="text-white dark:text-slate-950 bg-zinc-950 dark:bg-white min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-center">
       <main className="flex flex-col gap-8 items-center justify-center">
         {" "}
-        {/* Se elimina sm:items-start */}==
+        {/* Se elimina sm:items-start */}
         <div>
           <h1 className="text-5xl md:text-7xl font-medium tracking-tight">
             #2025<span className="text-red-500">Live</span>

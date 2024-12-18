62c92f51f72bf6f7d0ed9360ebe70e049241c59d
Gerard Bourguett <gerard.bourguett@gmail.com>
2024-12-17 21:55:05 -0300

    Second commit

---
+++ src/app/chao2024/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const Page = () => {
+  const [timeLeft, setTimeLeft] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    days: 0,
+  });
+
+  const countDown = () => {
+    const countDate = new Date("Dec 31, 2024 07:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    setTimeLeft({
+      days: Math.floor(gap / day),
+      hours: Math.floor((gap / hour) % 24),
+      minutes: Math.floor((gap / minute) % 60),
+      seconds: Math.floor((gap / second) % 60),
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(countDown, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="text-white dark:text-slate-950 bg-zinc-950 dark:bg-white grid items-center justify-items-center min-h-screen">
+      <main className="h-screen flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start">
+        <div className="text-center space-y-8">
+          <h1 className="text-5xl md:text-7xl font-medium tracking-tight">
+            #Chao<span className="text-red-500">2024</span>
+          </h1>
+
+          <div className="grid grid-cols-4 gap-4 items-center">
+            {["days", "hours", "minutes", "seconds"].map((unit, idx) => (
+              <div key={idx} className="flex flex-col items-center space-y-2">
+                <div className="bg-gray-800 rounded-lg p-4 shadow-lg transition-transform transform hover:scale-105">
+                  <span className="text-5xl md:text-6xl font-bold text-red-400">
+                    {timeLeft[unit].toString().padStart(2, "0")}
+                  </span>
+                </div>
+                <span className="text-sm md:text-lg uppercase font-medium text-gray-400">
+                  {unit.charAt(0).toUpperCase() + unit.slice(1)}
+                </span>
+              </div>
+            ))}
+          </div>
+
+          <Button className="text-center text-lg md:text-xl">
+            <Link href={"/"}>#2025Live</Link>
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl md:text-7xl font-medium tracking-tight">#2025<span className="text-red-500">Live</span></h1>
-        <a href="/chao2024" class="text-center text-lg md:text-xl mt-4 mb-4"
-      ><span
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >#RoadTo2025</span
-      ></a
-    >
-    <a
-      href="https://www.twitch.tv/vanderfondi"
-      target="_blank"
-      className="text-center text-lg md:text-xl mt-4"
-      ><span
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded block"
-        >Live on Dec 31, 2024 10:00 UTC</span
-      ></a
-    >
+    <div className="text-white dark:text-slate-950 bg-zinc-950 dark:bg-white min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-center">
+      <main className="flex flex-col gap-8 items-center justify-center">
+        {" "}
+        {/* Se elimina sm:items-start */}
+        <div>
+          <h1 className="text-5xl md:text-7xl font-medium tracking-tight">
+            #2025<span className="text-red-500">Live</span>
+          </h1>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <Button className="text-center text-lg md:text-xl mt-4 mb-4">
+            <Link href="/chao2024">#RoadTo2025</Link>
+          </Button>
+          <Button
+            className="text-center text-lg md:text-xl mt-4 mb-4"
+            variant={"destructive"}
+            disabled
+          >
+            <Loader2 className="animate-spin" /> Dec 31, 2024 10:00 UTC
+          </Button>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -67,7 +73,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }

f06e0c9fa4d0406bdf9afea4a6848c534611ead8
gbourguett <gbourguett@lovalledor.cl>
2024-12-24 12:17:15 -0300

    Ajuste grid /watch

--- src/components/StreamCard.tsx
+++ src/components/StreamCard.tsx
@@ -8,11 +8,11 @@ type StreamCardProps = {
 
 const StreamCard: FC<StreamCardProps> = ({ title, country, link }) => {
   return (
-    <div className="bg-gray-800 p-4 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold text-white mb-2">{title}</h2>
-      <p className="text-gray-400 mb-4">{country}</p>
+    <div className="bg-gray-800 p-4 rounded-lg shadow-md border-red-500">
+      <h2 className="text-xl font-semibold text-white mb-2">{country}</h2>
+      <p className="text-gray-400 mb-4">{title}</p>
       <iframe
-        width="560"
+        width="100%"
         height="315"
         src={link}
         title={title}
@@ -20,6 +20,7 @@ const StreamCard: FC<StreamCardProps> = ({ title, country, link }) => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
         className="rounded-lg"
+        sandbox=""
       ></iframe>
     </div>
   );
--- src/components/StreamGrid.tsx
+++ src/components/StreamGrid.tsx
@@ -54,9 +54,9 @@ const StreamGrid = () => {
   }
 
   return (
-    <div>
+    <div className="container mx-auto px-4">
       <h1 className="text-3xl font-bold mb-4">Live Streams</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
         {streams.length > 0 ? (
           streams.map((stream) => (
             <StreamCard
